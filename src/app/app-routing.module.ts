6865ef555df611caddc20f650dfb1f5f69adb29f
Lucas Antonio Ramos Sartori <lucassartori@cookiecode.com.br>
2020-10-25 10:24:26 -0300

    adde search structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)}
+  {path: '', pathMatch: 'full', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)},
+  {path: 'users', loadChildren: () => import('./user/user.module').then((m) => m.UserModule)},
+  {path: 'search', loadChildren: () => import('./search/search.module').then((m) => m.SearchModule)},
 ];
 
 @NgModule({
